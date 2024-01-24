5311a9180a086fbd8ebb5a7a306bd895104cd99a
Евдокимов Игорь Алексеевич <IAlekEvdokimov@sberbank.ru>
2024-01-24 12:26:37 +0300

    added call of access log on POST_READ phase

--- src/http/modules/ngx_http_log_module.c
+++ src/http/modules/ngx_http_log_module.c
@@ -1898,6 +1898,13 @@ ngx_http_log_init(ngx_conf_t *cf)
 
     cmcf = ngx_http_conf_get_module_main_conf(cf, ngx_http_core_module);
 
+    h = ngx_array_push(&cmcf->phases[NGX_HTTP_POST_READ_PHASE].handlers);
+    if (h == NULL) {
+        return NGX_ERROR;
+    }
+
+    *h = ngx_http_log_handler;
+
     h = ngx_array_push(&cmcf->phases[NGX_HTTP_LOG_PHASE].handlers);
     if (h == NULL) {
         return NGX_ERROR;
